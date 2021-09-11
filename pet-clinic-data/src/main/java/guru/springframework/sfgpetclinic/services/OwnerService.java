9b790b383df3bd1da3678bf779467c8de7aa0b36
Assaf Grossman <assafgrossman5@gmail.com>
2021-09-11 20:13:46 +0300

    added json endpoint. closes #75

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerService.java
@@ -8,6 +8,6 @@ import guru.springframework.sfgpetclinic.model.Owner;
  */
 public interface OwnerService extends CrudService<Owner, Long> {
 
-    Owner findByLastName(String lastName);
+    Owner findByLastName(String lastName);//bla bla
 
 }
